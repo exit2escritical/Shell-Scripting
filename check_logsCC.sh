0b190f3bacde1c8b8495a9dae24e3c4e38b23158
exit2escritical <exit2escritical@gmail.com>
2017-07-06 10:17:35 +0200

    Refactor critical and warning functions
    Instead of creating two functions to do the same thing, depending on severity, I use a loop to decide variables to pass when calling just one function.
    CheckCrit and CheckWarng -> CheckSeverity
    CheckExceptionsOnCritLogLines and CheckExceptionsOnWarnLogLines -> CheckException
    ExitCritState and ExitWarnState -> ExitState

--- check_logsCC.sh
+++ check_logsCC.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # ./check_logsCC  -F /tmp/messages -O /tmp/temporalmessages -c criticalpattern -w warningpattern -ce criticalexception -we warningexception
- 
+
 SCRIPT_NAME=`basename $0`
 PLUGING_PERF_DATA="0"
 PATH="/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin"
 export PATH
 SCRIPT_PATH=`echo $0 | sed -e 's,[\\/][^\\/][^\\/]*$,,'`
-REVISION="0.1"
+REVISION="0.3"
 
 STATE_OK=0
 STATE_WARNING=1
@@ -15,9 +15,9 @@ STATE_UNKNOWN=3
 STATE_DEPENDENT=4
 
 print_usage() {
-    echo "Usage: $SCRIPT_NAME -F logfile -O oldlog -q query -e exception"
-    echo "Usage: $SCRIPT_NAME --help"
-    echo "Usage: $SCRIPT_NAME --version"
+    echo "Usage: $SCRIPT_NAME -F logfile -O oldlog -c CriticalPattern -ce Criticalexception -w WarningPattern -we WarningException"
+    echo "Usage: $SCRIPT_NAME -h"
+    echo "Usage: $SCRIPT_NAME -V"
 }
 
 print_help() {
@@ -32,7 +32,7 @@ print_help() {
 
 # Make sure the correct number of command line
 # arguments have been supplied
-if [ $# -lt 6 ]; then
+if [ $# -lt 3 ]; then
     print_usage
     exit $STATE_UNKNOWN
 fi
@@ -40,76 +40,36 @@ fi
 # Grab the command line arguments
 while test -n "$1"; do
     case "$1" in
-        --help)
-            print_help
-            exit $STATE_OK
-            ;;
         -h)
             print_help
-            exit $STATE_OK
-            ;;
-        --version)
-            print_revision $SCRIPT_NAME $REVISION
             exit $STATE_OK
             ;;
         -V)
             print_revision $SCRIPT_NAME $REVISION
             exit $STATE_OK
             ;;
-        --filename)
-            logfile=$2
-            shift
-            ;;
         -F)
             logfile=$2
-            shift
-            ;;
-        --oldlog)
-            oldlog=$2
             shift
             ;;
         -O)
             oldlog=$2
             shift
             ;;
-        --critical)
-            critical=$2
-            shift
-            ;;
         -c)
             critical=$2
-            shift
-            ;;
-        --critExcept)
-            critExcept=$2
             shift
             ;;
         -ce)
             critExcept=$2
             shift
             ;;
-        --warning)
-            warning=$2
-            shift
-            ;;
         -w)
             warning=$2
             shift
             ;;
-        --warnExcept)
-            warnExcept=$2
-            shift
-            ;;
         -we)
             warnExcept=$2
-            shift
-            ;;
-        -x)
-            exitstatus=$2
-            shift
-            ;;
-        --exitstatus)
-            exitstatus=$2
             shift
             ;;
         *)
@@ -121,7 +81,7 @@ while test -n "$1"; do
     shift
 done
 
-# If the source log file doesn't exist, exit
+
 CheckIfSourceLogExists(){
 if [ ! -e $logfile ]; then
     echo "Log check error: Log file $logfile does not exist!"
@@ -167,98 +127,80 @@ DeleteTmpFilesAndUpdOldLog(){
         rm -f "$tempWarn"
         rm -f "$CriticalAfterExcept"
         rm -f "$WarningAfterExcept"
-
 }
 
-
-CheckCrit(){
-countCrit=`grep -c "$critical" $tempdiff`
-if [ $countCrit -gt 0 ];then
+CheckSeverity(){
+count=`grep -c "$pattern" $tempdiff`
+if [ $count -gt 0 ];then
     while read line
     do
-        IsCrit=`echo "$line" | grep -c "$critical"`
-        if [ "$IsCrit" -eq 1 ];then
-            echo "$line" >> $tempCrit
+        FoundLine=`echo "$line" | grep -c "$pattern"`
+        if [ "$FoundLine" -eq 1 ];then
+            echo "$line" >> $tempSeverity
         fi
     done < $tempdiff
 fi
 }
 
-CheckWarn(){
-countWarn=`grep -c "$warning" $tempdiff`
-if [ $countWarn -gt 0 ];then
-    while read line
-    do
-        IsWarn=`echo "$line" | grep -c "$warning"`
-        if [ "$IsWarn" -eq 1 ];then
-            echo "$line" >> $tempWarn
-        fi
-    done < $tempdiff
-fi
-}
-
-
-CheckExceptionsOnCritLogLines(){
-for i in $(echo `seq 1 $countCrit`)
+CheckException(){
+for i in $(echo `seq 1 $count`)
 do
-        line=`sed -n ''"$i"'p' $tempCrit`
-        HasCExcept=`echo "$line" | grep -c "${critExcept}" || true`
-        if [ "$HasCExcept" = 0 ];then
-            echo "$line" >> $CriticalAfterExcept
+        line=`sed -n ''"$i"'p' $tempSeverity`
+        HasExcept=`echo "$line" | grep -c "${Except}" || true`
+        if [ "$HasExcept" = 0 ];then
+            echo "$line" >> $AfterExcept
         fi
 done
 }
 
-CheckExceptionsOnWarnLogLines(){
-for i in $(echo `seq 1 $countWarn`)
-do
-        line=`sed -n ''"$i"'p' $tempWarn`
-        HasWExcept=`echo "$line" | grep -c "${warnExcept}" || true`
-        if [ "$HasWExcept" = 0 ];then
-            echo "$line" >> $WarningAfterExcept
-        fi
-done
-}
-
-ExitCritState(){
-NumberCritFound=`wc -l ${CriticalAfterExcept} | awk '{print $1}'`
-EchoCLines="$(cat "${CriticalAfterExcept}" )"
-PLUGING_PERF_DATA="$NumberCritFound"
-if [ $NumberCritFound -ne 0 ];then
-    PLUGIN_OUTPUT_MSG="[$NumberCritFound] lines match critical pattern after exceptions [$EchoCLines]"
-    echo "$PLUGIN_OUTPUT_MSG | $PLUGING_PERF_DATA" && exit $STATE_CRITICAL
+ExitState(){
+NumberLinesFound=`wc -l ${AfterExcept} | awk '{print $1}'`
+EchoLines="$(cat "${AfterExcept}" )"
+PLUGING_PERF_DATA="$NumberLinesFound"
+if [ $NumberLinesFound -ne 0 ];then
+    PLUGIN_OUTPUT_MSG="[$NumberLinesFound] lines match severity pattern after exceptions [$EchoLines]"
+    echo "$PLUGIN_OUTPUT_MSG | $PLUGING_PERF_DATA" && exit $STATE_SEVERITY
 fi
 }
-
-ExitWarnState(){
-NumberWarnFound=`wc -l ${WarningAfterExcept} | awk '{print $1}'`
-EchoWLines="$(cat "${WarningAfterExcept}" )"
-PLUGING_PERF_DATA="$NumberWarnFound"
-if [ $NumberWarnFound -ne 0 ];then
-    PLUGIN_OUTPUT_MSG="[$NumberWarnFound] lines match warning pattern after exceptions [$EchoWLines]"
-    echo "$PLUGIN_OUTPUT_MSG | $PLUGING_PERF_DATA" && exit $STATE_WARNING
-fi
-}
-
 
 CheckIfSourceLogExists
 CheckIfOldLogExists
-trap DeleteTmpFilesAndUpdOldLog EXIT
 CreateTempFiles
-CheckCrit
-CheckWarn
-if [ "$countWarn" = 0 ] && [ "$countCrit" = 0 ];then
-    echo "OK | $PLUGING_PERF_DATA" && exit $STATE_OK
-else
-    if [ -z "${critExcept}" ];then 
-        cp $tempdiff $CriticalAfterException
-    elif [ -z "${warnExcept}" ];then 
-        cp $tempdiff $WarningAfterExcept
-    else 
-        CheckExceptionsOnCritLogLines
-        CheckExceptionsOnWarnLogLines
-    fi
+trap DeleteTmpFilesAndUpdOldLog EXIT
+
+if [ ! -z "$critical" ]; then
+        pattern=$critical
+        tempSeverity=$tempCrit
+        CheckSeverity
+        if [ "$count" != 0 ]; then
+                Except=$critExcept
+                AfterExcept=$CriticalAfterExcept
+                STATE_SEVERITY=$STATE_CRITICAL
+                if [ -z "${critExcept}" ];then
+                        cp $tempdiff $CriticalAfterException
+                else
+                        CheckException
+                        ExitState
+                fi
+        fi
 fi
 
-ExitCritState
-ExitWarnState
+if [ ! -z "$warning" ]; then
+        pattern=$warning
+        tempSeverity=$tempWarn
+        CheckSeverity
+        if [ "$count" != 0 ]; then
+                Except=$warnExcept
+                AfterExcept=$WarningAfterExcept
+                STATE_SEVERITY=$STATE_WARNING
+                if [ -z "${warnExcept}" ];then
+                        cp $tempdiff $WarningAfterException
+                else
+                        CheckException
+                fi
+        else 
+                exit $STATE_OK
+        fi
+fi
+
+ExitState
